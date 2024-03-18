052f97857d4f51c40e0becdc6e44d886e6e9d038
danildenha <danildenha@gmail.com>
2024-03-18 13:02:08 -0500

    changes

---
+++ VisionApp/ViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ViewModel.swift
+//  VisionApp
+//
+//  Created by Danil Denha on 3/18/24.
+//
+
+import Foundation
+import Observation
+import SwiftUI
+
+enum FlowState {
+    case idle
+    case intro
+    case projectileFlying
+    case updateWallArt
+}
+
+@Observable
+class ViewModel {
+    
+    var flowState = FlowState.idle
+    
+}
