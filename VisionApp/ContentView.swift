1ba1b9f22662aa29c652ce7b88563715f13becd9
danildenha <danildenha@gmail.com>
2024-03-17 23:00:44 -0500

    model3d

--- VisionApp/ContentView.swift
+++ VisionApp/ContentView.swift
@@ -51,23 +51,6 @@ struct WelcomeView: View {
         }
     }
 }
-struct SphereView: View {
-    var scale = false
-
-
-    var body: some View {
-        RealityView { content in
-            let model = ModelEntity(
-                         mesh: .generateSphere(radius: 0.1),
-                         materials: [SimpleMaterial(color: .white, isMetallic: true)])
-            content.add(model)
-        } update: { content in
-            if let model = content.entities.first {
-                model.transform.scale = scale ? [1.2, 1.2, 1.2] : [1.0, 1.0, 1.0]
-            }
-        }
-    }
-}
 
 struct ContentView: View {
     @State private var gifUrl: URL?
